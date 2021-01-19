e1b3680552f3d2dc22edc22eb65073e7b335a2a5
SanFable <SanFable@gmail.com>
2021-01-19 21:28:58 +0100

    added i2c scanner, set up connection with 2 sensors

--- main.cpp
+++ main.cpp
@@ -5,40 +5,79 @@
 #define VL53L1_I2C_SDA   PA_10
 #define VL53L1_I2C_SCL   PA_9
 
+VL53L1X_DevI2C *device_i2c = new VL53L1X_DevI2C(VL53L1_I2C_SDA, VL53L1_I2C_SCL);
 
- 
+
+ void checkaddrs(){
+     I2C i2c(PA_10, PA_9); 
+     int error, address;
+     for(address = 0; address < 128; address++ ){
+        thread_sleep_for(10);
+        error = i2c.write(address  << 1, "1", 1);
+
+        if (error == 0){
+            printf("I2C device found at address 8bit: 0x%X 7bit: 0x%X\n", address, address << 1);
+
+        }
+    }
+ }
 int main()
 {
 
-    VL53L1X_DevI2C *device_i2c = new VL53L1X_DevI2C(VL53L1_I2C_SDA, VL53L1_I2C_SCL);
+    
 
-static VL53L1X *sensor = NULL;
-uint16_t TimingBudgetInMs = 15;
+    static VL53L1X *sensor[5] = {NULL};
+    uint16_t TimingBudgetInMs = 15;
+    printf("before init\n");
+    checkaddrs();
+    uint32_t piData[5] = {NULL};
+    uint8_t isDataReady[5] = {0};
+    DigitalOut xshutdown[2]{PB_4,PB_5};
+    sensor[0] = new VL53L1X(device_i2c, &xshutdown[0], PA_3);
+    sensor[1] = new VL53L1X(device_i2c, &xshutdown[1], PA_3);
+
+	sensor[0]->init_sensor(0x54);
+    sensor[1]->init_sensor(0x56);
+
+    printf("after init\n");
+    checkaddrs();
     
-    uint32_t piData = NULL;
-    uint8_t isDataReady = 1;
-    DigitalOut xshutdown(PB_4);
-    sensor = new VL53L1X(device_i2c, &xshutdown, PA_3);
- 
-    sensor->vl53l1_off();
-    /* initialise sensor */
-    sensor->init_sensor(0x52);
-    //range_measure(device_i2c, 0);  // start continuous measures
+
+    sensor[0]->vl53l1x_set_timing_budget_in_ms(TimingBudgetInMs);
+	sensor[0]->vl53l1x_set_inter_measurement_in_ms(TimingBudgetInMs);
+    sensor[0]->vl53l1x_set_distance_mode(1);
+    sensor[0]->vl53l1x_start_ranging();
     
-    sensor->vl53l1x_set_timing_budget_in_ms(TimingBudgetInMs);
-	sensor->vl53l1x_set_inter_measurement_in_ms(TimingBudgetInMs);
-    sensor->vl53l1x_set_distance_mode(1);
-    sensor->vl53l1x_start_ranging();
+    sensor[1]->vl53l1x_set_timing_budget_in_ms(TimingBudgetInMs);
+	sensor[1]->vl53l1x_set_inter_measurement_in_ms(TimingBudgetInMs);
+    sensor[1]->vl53l1x_set_distance_mode(1);
+    sensor[1]->vl53l1x_start_ranging();  
+    
 
 while(1){
-while(isDataReady){
-    sensor->vl53l1x_check_for_data_ready(&isDataReady);
-    printf("waiting\n");
+if (!isDataReady[0]){ 
+    sensor[0]->vl53l1x_check_for_data_ready(&isDataReady[0]);
 }
-    sensor->get_distance(&piData);
-printf("dist: %d \n", piData);
-    sensor->vl53l1x_clear_interrupt();
+else{
+    isDataReady[0] = 0;
+    sensor[0]->get_distance(&piData[0]);
+    printf("sens 0: %d", piData[0]);
+    sensor[0]->vl53l1x_clear_interrupt();
+}
 
+
+if (!isDataReady[1]){ 
+    sensor[1]->vl53l1x_check_for_data_ready(&isDataReady[1]);
+}
+else{
+    isDataReady[1] = 0;
+    sensor[1]->get_distance(&piData[1]);
+    printf(" sens 1: %d\n", piData[1]);
+    sensor[1]->vl53l1x_clear_interrupt();
+}
+
+
+wait_us(100000);
 }
     return 0;
 }
