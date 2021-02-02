d602b9542b9cc9f677151802699ba04990bbd6b9
SanFable <SanFable@gmail.com>
2021-02-02 18:34:32 +0100

    Continuing moving to objective cpp

---
+++ nucleodetectingpeople/CSensorManager.h
@@ -0,0 +1,20 @@
+#include "CSensorBase.h"
+#include "VL53L1X_I2C.h"
+#include "VL53L1X_Class.h"
+class CSensorManager
+{
+public:
+    CSensorManager();
+    ~CSensorManager();
+    int Init()
+    {
+        oSensorVector.add(CSensorVL53L1X(device_i2c, &xshutdown, PA_3));
+        oSensorVector.add(CSensorVL53L1X(device_i2c, &xshutdown, PA_3));
+    }
+
+
+private:
+    vector<CSensorBase> oSensorVector;
+
+
+}
