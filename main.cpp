886f68ccde0a681035ab6c32c70762a55ea2a276
SanFable <SanFable@gmail.com>
2021-03-28 13:37:11 +0200

    measuring 6 zones, problems with more sensors, probabbly hardware

--- main.cpp
+++ main.cpp
@@ -16,14 +16,17 @@ uint8_t sensorAdress;
 VL53L1X *obj;
 uint32_t tempData;
 uint8_t isDataReady;
-vector<uint32_t> sensorData;
+vector<uint16_t> sensorData;
 };
 Sensor sensors[numberOfSensors];
 Thread thread[numberOfSensors];
 DigitalOut xshutdown[numberOfSensors]{PB_4, PB_5, PA_11, PA_8, PB_0, PB_7, PB_6, PB_1};
 uint8_t sensorAdress[numberOfSensors]{0x54, 0x56, 0x58, 0x60, 0x62, 0x64, 0x66, 0x68};
+uint8_t roiArray[]{151, 167, 183, 199, 215, 239};
+uint8_t roiX = 4;
+uint8_t roiY = 16;
 int sensorNumber[numberOfSensors]{0, 1, 2, 3, 4, 5, 6, 7};
-uint8_t timingBudgetInMs = 15;
+uint8_t timingBudgetInMs = 20;
 uint8_t shortDistanceMode = 1;
  void checkaddrs(){
      I2C i2c(PA_10, PA_9); 
@@ -39,21 +42,29 @@ uint8_t shortDistanceMode = 1;
     }
  }
 
-void getdata( int *i){
+void getROIData( int *i){
+uint8_t zoneMeasured = 0;
     while (1){
+        sensors[*i].obj->VL53L1X_SetROICenter(roiArray[zoneMeasured]); //cos z tym rozmiarem bo to jest rozmiar a nie gdzie
+        wait_us(10000);
         while (!sensors[*i].isDataReady){ 
             sensors[*i].obj->vl53l1x_check_for_data_ready(&sensors[*i].isDataReady);
         wait_us(3000);
         }
     sensors[*i].isDataReady = 0;
     sensors[*i].obj->get_distance(&sensors[*i].tempData);
-    //printf("\nsens %d: %d\n", *i, sensors[*i].tempData);
-    sensors[*i].sensorData.push_back(sensors[*i].tempData);
-    printf("sens %d size: %d\n", *i, sensors[*i].sensorData.size());
+    printf("\nsens %d: %d zone: %d\n", *i, sensors[*i].tempData, zoneMeasured);
+    //sensors[*i].sensorData.push_back(sensors[*i].tempData);
+    //printf("sens %d dist: %d size: %d\n", *i, sensors[*i].tempData, sensors[*i].sensorData.size());
     sensors[*i].obj->vl53l1x_clear_interrupt();
-    wait_us(100000);
+    
+    zoneMeasured++;
+    if (zoneMeasured==6)
+        zoneMeasured = 0;
+    //wait_us(100000);
     }
 }
+
 
 void preInitSensors(){
 for (uint8_t i = 0;i<numberOfSensors;i++){
@@ -67,6 +78,7 @@ sensors[i].obj->init_sensor(sensorAdress[i]);
 sensors[i].obj->vl53l1x_set_timing_budget_in_ms(timingBudgetInMs);
 sensors[i].obj->vl53l1x_set_inter_measurement_in_ms(timingBudgetInMs);
 sensors[i].obj->vl53l1x_set_distance_mode(shortDistanceMode);
+sensors[i].obj->vl53l1x_set_roi(roiX,roiY);
 sensors[i].obj->vl53l1x_start_ranging();
 }
 
@@ -76,16 +88,15 @@ int main()
     
 
     
- checkaddrs();
+ 
     printf("before init\n");
     preInitSensors();
     wait_us(10000);
     initSingleSensor(sensorNumber[0]);
     wait_us(10000);
-    initSingleSensor(sensorNumber[1]);
-    
-thread[sensorNumber[0]].start(callback(getdata,&sensorNumber[0]));
-thread[sensorNumber[1]].start(callback(getdata,&sensorNumber[1]));
-
+  //  initSingleSensor(sensorNumber[7]);
+    checkaddrs();
+thread[sensorNumber[0]].start(callback(getROIData,&sensorNumber[0]));
+//thread[sensorNumber[7]].start(callback(getROIData,&sensorNumber[7]));
 
 }
