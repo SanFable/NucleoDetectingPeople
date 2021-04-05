ba8d179403b1a3c42a867f788d0a00bac5468a04
SanFable <SanFable@gmail.com>
2021-04-05 17:07:14 +0200

    Merged to DISCO_F746NG, dirty LCD usage

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 #include "mbed.h"
 #include "VL53L1X_I2C.h"
 #include "VL53L1X_Class.h"
+#include "stm32746g_discovery_lcd.h"
+#include "stm32746g_discovery_ts.h"
 #include <vector>
-#define VL53L1_I2C_SDA   PA_10
-#define VL53L1_I2C_SCL   PA_9
+#define VL53L1_I2C_SDA   PB_9
+#define VL53L1_I2C_SCL   PB_8
 VL53L1X_DevI2C *device_i2c = new VL53L1X_DevI2C(VL53L1_I2C_SDA, VL53L1_I2C_SCL);
 const uint8_t numberOfSensors = 8;
 Timer t;
+
 struct Sensor{
 DigitalOut *xshutdown;
 PinName interrupt;
@@ -18,14 +21,13 @@ VL53L1X *obj;
 uint32_t tempData;
 uint8_t isDataReady;
 uint16_t sensorData[2][1000]{0};
-uint32_t timestampData[2][100]{0};
+uint32_t timestampData[2][1000]{0};
 uint8_t whichArray = 0;
 uint16_t numofData = 0;
-
 };
 Sensor sensors[numberOfSensors];
 Thread thread[numberOfSensors];
-DigitalOut xshutdown[numberOfSensors]{PB_4, PB_5, PA_11, PA_8, PB_0, PB_7, PB_6, PB_1};
+DigitalOut xshutdown[numberOfSensors]{PC_7, PC_6, PB_4, PG_7, PH_6, PI_3, PI_2, PA_15};
 uint8_t sensorAdress[numberOfSensors]{0x54, 0x56, 0x58, 0x60, 0x62, 0x64, 0x66, 0x68};
 uint8_t roiArray[]{151, 167, 183, 199, 215, 239};
 uint8_t roiX = 4;
@@ -33,8 +35,10 @@ uint8_t roiY = 16;
 int sensorNumber[numberOfSensors]{0, 1, 2, 3, 4, 5, 6, 7};
 uint8_t timingBudgetInMs = 20;
 uint8_t shortDistanceMode = 1;
+uint8_t text[numberOfSensors][30];
+
  void checkaddrs(){
-     I2C i2c(PA_10, PA_9); 
+     I2C i2c(VL53L1_I2C_SDA, VL53L1_I2C_SCL); 
      int error, address;
      for(address = 0; address < 128; address++ ){
         thread_sleep_for(10);
@@ -51,18 +55,27 @@ void getROIData( int *i){
 uint8_t zoneMeasured = 0;
     while (1){
         sensors[*i].obj->VL53L1X_SetROICenter(roiArray[zoneMeasured]); 
-        wait_us(10000);
+        //wait_us(20000);
         while (!sensors[*i].isDataReady){ 
             sensors[*i].obj->vl53l1x_check_for_data_ready(&sensors[*i].isDataReady);
-        wait_us(3000);
+        ThisThread::sleep_for(3ms);
         }
     sensors[*i].isDataReady = 0;
     sensors[*i].obj->get_distance(&sensors[*i].tempData);
-    //printf("sens %d: %d zone: %d\n", *i, sensors[*i].tempData, zoneMeasured);
-    
+    printf("sens %d: %d zone: %d\n", *i, sensors[*i].tempData, zoneMeasured);
+    //
+    if(*i % 2 == 0) {
+        sprintf((char*)text[*i], "dist: %d", sensors[*i].tempData);
+        BSP_LCD_DisplayStringAt(0, 0, (uint8_t *)&text[*i], RIGHT_MODE); }
+    if(*i % 2 == 1)
+    {
+        sprintf((char*)text[*i], "dist: %d", sensors[*i].tempData);
+        BSP_LCD_DisplayStringAt(0, 90, (uint8_t *)&text[*i], RIGHT_MODE); }
+
+    ThisThread::sleep_for(100ms);
     sensors[*i].sensorData[sensors[*i].whichArray][sensors[*i].numofData] = sensors[*i].tempData;
-    printf("array %d val %d index %d\n", sensors[*i].whichArray, sensors[*i].sensorData[sensors[*i].whichArray][sensors[*i].numofData], sensors[*i].numofData);
-    //sensors[*i].timestampData[sensors[*i].whichArray][sensors[*i].numofData] = (duration_cast<std::chrono::milliseconds>(t.elapsed_time()).count());
+    //printf("array %d val %d index %d\n", sensors[*i].whichArray, sensors[*i].sensorData[sensors[*i].whichArray][sensors[*i].numofData], sensors[*i].numofData);
+    sensors[*i].timestampData[sensors[*i].whichArray][sensors[*i].numofData] = (duration_cast<std::chrono::milliseconds>(t.elapsed_time()).count());
        // printf("     The time taken was %llu milliseconds\n", duration_cast<std::chrono::milliseconds>(t.elapsed_time()).count());
   
     //sensors[*i].sensorData.push_back(sensors[*i].tempData);
@@ -107,20 +120,31 @@ sensors[i].obj->vl53l1x_start_ranging();
 int main()
 {
 t.start();
+BSP_LCD_Init();
+    BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, LCD_FB_START_ADDRESS);
+    BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
+    BSP_LCD_Clear(LCD_COLOR_BLACK);
+    BSP_LCD_SetFont(&LCD_DEFAULT_FONT);
+    BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
+    BSP_LCD_SetTextColor(LCD_COLOR_ORANGE);
+    //BSP_LCD_DisplayStringAt(0, 1, (uint8_t *)"ELOooo", CENTER_MODE);
 
     
 
-    
+    int a = 0;
+    int b = 1;
  
     printf("before init\n");
     preInitSensors();
     wait_us(10000); 
-    initSingleSensor(sensorNumber[0]);
-    wait_us(10000);
-  // initSingleSensor(sensorNumber[3]);
-    checkaddrs();
+        
+    initSingleSensor(sensorNumber[a]);
+    wait_us(1000000);
+    initSingleSensor(sensorNumber[b]);
+
+checkaddrs();
     
-thread[sensorNumber[0]].start(callback(getROIData,&sensorNumber[0]));
-//thread[sensorNumber[3]].start(callback(getROIData,&sensorNumber[3]));
+thread[sensorNumber[a]].start(callback(getROIData,&sensorNumber[a]));
+thread[sensorNumber[b]].start(callback(getROIData,&sensorNumber[b]));
 
 }
